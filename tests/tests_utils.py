0ce06ae7edacb8e286f7811f240da296523e44ab
екатерина функ <funkkatysha@gmail.com>
2024-06-20 18:05:03 +0700

    corrected dz

--- tests/tests_utils.py
+++ tests/tests_utils.py
@@ -8,18 +8,18 @@ from src.utils import get_transactions_from_json
 def test_get_transactions_from_json(mock_open):
     mock_file = mock_open.return_value.__enter__.return_value
 
-    # Проверка на удачный результат
+    # Проверка на удачный результат.
     mock_file.read.return_value = json.dumps([{"test": "test"}])
     assert get_transactions_from_json("test.json") == [{"test": "test"}]
 
-    # Проверка на ошибку типа файла
+    # Проверка на ошибку типа файла.
     mock_file.read.return_value = json.dumps({})
     assert get_transactions_from_json("test.json") == []
 
-    # Проверка на некорректный файл
+    # Проверка на некорректный файл.
     mock_file.read.return_value = json.dumps("testtest")
     assert get_transactions_from_json("test.json") == []
 
-    # Проверка на пустой файл
+    # Проверка на пустой файл.
     mock_file.read.return_value = ""
     assert get_transactions_from_json("test.json") == []
