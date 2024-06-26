44f4d92a28db7ef893748147c69684c9a75d2f4e
екатерина функ <funkkatysha@gmail.com>
2024-06-26 19:51:26 +0700

    homework_13_1 done

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,7 @@
 import json
 from unittest.mock import patch
 
-from src.utils import get_transactions_from_json
+from src.utils import get_transactions_from_file
 
 
 @patch("builtins.open")
@@ -10,16 +10,16 @@ def test_get_transactions_from_json(mock_open):
 
     # Проверка на удачный результат.
     mock_file.read.return_value = json.dumps([{"test": "test"}])
-    assert get_transactions_from_json("test.json") == [{"test": "test"}]
+    assert get_transactions_from_file("test.json") == [{"test": "test"}]
 
     # Проверка на ошибку типа файла.
     mock_file.read.return_value = json.dumps({})
-    assert get_transactions_from_json("test.json") == []
+    assert get_transactions_from_file("test.json") == []
 
     # Проверка на некорректный файл.
     mock_file.read.return_value = json.dumps("testtest")
-    assert get_transactions_from_json("test.json") == []
+    assert get_transactions_from_file("test.json") == []
 
     # Проверка на пустой файл.
     mock_file.read.return_value = ""
-    assert get_transactions_from_json("test.json") == []
+    assert get_transactions_from_file("test.json") == []
