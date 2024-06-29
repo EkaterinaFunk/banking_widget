8d973db10b7d0f70c1b6c07838bcf51a321daa19
екатерина функ <funkkatysha@gmail.com>
2024-06-29 19:55:33 +0700

    tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -23,3 +23,17 @@ def test_get_transactions_from_json(mock_open):
     # Проверка на пустой файл.
     mock_file.read.return_value = ""
     assert get_transactions_from_file("test.json") == []
+
+
+@patch("pandas.read_csv")
+def test_get_transactions_from_csv(mock_read_csv, transactions):
+    mock_read_csv.return_value.to_dict.return_value = transactions
+    assert get_transactions_from_file("data/transactions.csv") == transactions
+    mock_read_csv.assert_called_once_with("data/transactions.csv", encoding="utf8")
+
+
+@patch("pandas.read_excel")
+def test_get_transactions_from_excel(mock_read_excel, transactions):
+    mock_read_excel.return_value.to_dict.return_value = transactions
+    assert get_transactions_from_file("data/transactions.xlsx") == transactions
+    mock_read_excel.assert_called_once_with("data/transactions.xlsx")
